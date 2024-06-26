900fb2cc94913b7717db8cd770707de3db791158
excel <excel@LAPTOP-DQ7KB8NS>
2024-06-26 18:12:24 +0530

    3rd commit

--- src/test/java/com/testcases/udemy/Sign_upTest.java
+++ src/test/java/com/testcases/udemy/Sign_upTest.java
@@ -1,7 +1,36 @@
 package com.testcases.udemy;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 import com.Base.Udemy.Base;
 
-public class Sign_upTest  extends Base{
+public class Sign_upTest extends Base {
+@Test(priority = 1)
+	public void  sigupButton() throws InterruptedException {
+		            // Click on the Sign Up button to navigate to the sign-up page
+		            WebElement signUpButton = driver.findElement(By.linkText("Sign up"));
+		            signUpButton.click();
+
+		            // Wait for the sign-up form to load (you might need to add explicit wait here)
+                 Thread.sleep(2000);
+
+		            // Locate and fill the sign-up form fields
+		            WebElement nameField = driver.findElement(By.name("fullname"));
+		            WebElement emailField = driver.findElement(By.name("email"));
+		            WebElement passwordField = driver.findElement(By.name("password"));
+		            WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+
+		            // Fill the form
+		            nameField.sendKeys("Neelam-Kushwaha");
+		            emailField.sendKeys("neelameKw24@gmail.com");
+		            passwordField.sendKeys("nick@345");
+
+		            // Submit the form
+		            submitButton.click();
+
+
+}
 
 }
--- src/test/java/com/testcases/udemy/filterCategory.java
+++ src/test/java/com/testcases/udemy/filterCategory.java
@@ -1,15 +1,29 @@
 package com.testcases.udemy;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.Base.Udemy.Base;
 
+
 public class filterCategory  extends Base{
 
 	@Test
-	public void filterCategoryCourse() {
+	public void filterCategoryCourse() throws InterruptedException {
+		WebElement Search = driver.findElement(By.name("q"));
+
+		Search.sendKeys("manual testing");
+		Search.submit();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//*[@id=\"blYgG5\"]/div/label/input")).click();
+		Thread.sleep(1000);
 		driver.findElement(By.xpath("//*[@id=\"main-content-anchor\"]/div/div/div[1]/div/div/div/div/div")).click();
-		
+		//Rating
+	WebElement RedioButton = driver.findElement(By.id("u99-radio--260"));
+	RedioButton.click();
+	//Lagaluge
+	WebElement CheckBox = driver.findElement(By.xpath("//*[@id=\"filter-form\"]/div/div[2]/div[2]/div/div/div/div/div/label[1]/input[1]"));
+	CheckBox.click();
 	}
 }
