b8cd2e42c91b44663f2a85232ccf53ba8e2e88d4
excel <excel@LAPTOP-DQ7KB8NS>
2024-07-01 15:29:03 +0530

    5th commit

--- src/main/java/com/Base/Udemy/Base.java
+++ src/main/java/com/Base/Udemy/Base.java
@@ -2,25 +2,36 @@ package com.Base.Udemy;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Base {
 
 public static	WebDriver driver;
 	@BeforeClass
-	public void Browseropen() {
-	driver = new ChromeDriver();
-	driver.get("https://www.udemy.com/");
-	driver.manage().deleteAllCookies();
-	driver.manage().window().maximize();
-	System.out.println(driver.getCurrentUrl());
-	System.out.println(driver.getTitle());
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 
+	driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void UrlLaunching() {
+		driver.get("https://www.udemy.com/");
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        System.out.println("Title: " + driver.getTitle());
 	}
 	@AfterClass
-	public void closebrowser() {
-		driver.close();
+	public void closeBrowser() {
+
+		if (driver != null) {
+            //driver.close();
+        }
 	}
 	}
+
 
